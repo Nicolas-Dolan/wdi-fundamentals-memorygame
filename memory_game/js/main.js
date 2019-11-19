7eb93c26544fdf5141ca649b65dec8fd2bd3c79c
Nicolas Dolan <nicolas.dolan@ymail.com>
2019-11-19 17:15:10 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,17 @@
-console.log("Up and running!");
-
 var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
+function checkForMatch () {if (cardsInPlay.length === 2) {
+if (cardsInPlay[0] === cardsInPlay[1]) {console.log("You found a match!");}
+else  {console.log("Sorry, try again.");};
+}};
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-
-if (cardsInPlay[0] === cardsInPlay[1]) {alert("You found a match!");}
-else  {alert("Sorry, try again.");};
+function flipCard (cardId) {
+ console.log ("User flipped " + cards[cardId]);
+ cardsInPlay.push(cards[cardId]);
+ checkForMatch();
 }
+
+flipCard(0);
+flipCard(2);
