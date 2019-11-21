76ed111d0c560fc0ac8da34785ae5510540febe2
Nicolas Dolan <nicolas.dolan@ymail.com>
2019-11-21 14:34:08 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,18 +23,30 @@ var cards = [
 
 var cardsInPlay = [];
 
-function checkForMatch () {if (cardsInPlay.length === 2) {
-if (cardsInPlay[0] === cardsInPlay[1]) {console.log("You found a match!");}
+function checkForMatch () {
+	if (cardsInPlay.length === 2) {
+if (cardsInPlay[0] === cardsInPlay[1]) {alert("You found a match!");}
 else  {alert("Sorry, try again.");};
 }};
 
-function flipCard (cardId) {
- console.log ("User flipped " + cards[cardId].rank);
+function flipCard () {
+	var cardId = this.getAttribute('data-id');
+ this.setAttribute('src', cards[cardId].cardImage);
+ console.log ("User flipped the " + cards[cardId].rank + " of " + cards[cardId].suit);
  console.log (cards[cardId].cardImage);
- console.log (cards[cardId].suit);
  cardsInPlay.push(cards[cardId].rank);
  checkForMatch();
-}
+};
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+}
+};
+
+
+createBoard();
